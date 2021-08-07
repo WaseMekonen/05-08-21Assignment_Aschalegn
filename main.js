a7cbe1670fbb58fe13a61f84d484e5d9a6bc79d9
MekonenWase <mekonen.wase@gmail.com>
2021-08-07 15:00:50 +0300

    home assignment

---
+++ main.js
@@ -0,0 +1,72 @@
+
+
+// var classAges = [24, 25, 34, 30, 20, 28];
+// var minAge = classAges[0];
+// var maxAge = classAges[0]
+// var sumOfAges = classAges[0];
+// var multiOfAges = classAges[0];
+
+// var userInput = Number(prompt("Insert a number:"));
+// for(var i=1; i<classAges.length; i++){
+//     if(classAges[i]<minAge){
+//         minAge = classAges[i];
+//     }else if(classAges[i]> maxAge){
+//         maxAge = classAges[i];
+//     }
+//     sumOfAges +=  classAges[i];
+//     multiOfAges *= classAges[i];
+//     if(userInput == classAges[0] || userInput == classAges[i]){
+//         alert("you have a match");
+//     }
+// }
+
+// console.log(minAge);
+// console.log(maxAge);
+// console.log(sumOfAges);
+// console.log(sumOfAges / classAges.length);
+// console.log(multiOfAges);
+
+////// ex- 1.7 ////////
+
+
+// for(var i=0; i<10; i++){
+//     for(j=0; j<classAges.length; j++){
+//         console.log(classAges[j]);
+//     }
+// }
+
+/////////// exercise 2: /////////////
+
+
+// var arrayOfItems = ["bonjor",24,"selam",45,"hello",88];
+// var arrayOfstrings = [];
+// var arrayOfNumbers = [];
+
+// for (var i=0; i<arrayOfItems.length; i++ ){
+//     if(Number(arrayOfItems[i])){
+//         arrayOfNumbers.push(arrayOfItems[i]);
+//     }else{
+//         arrayOfstrings.push(arrayOfItems[i]);
+//     }
+// }
+// console.log(arrayOfNumbers);
+// console.log(arrayOfstrings);
+
+
+///////////// exercise 3: ////////////
+
+
+// var arrayOfcars = [56,"Lamborgini",23, 'BMW',67,"Mercedes","Feirari","Lexus"];
+
+// var temp = arrayOfcars[0]
+// var temp2 = arrayOfcars[1]
+// var temp3 = arrayOfcars[arrayOfcars.length-1]
+// arrayOfcars[0] = temp3;
+// arrayOfcars[arrayOfcars.length-1] =temp;
+
+// console.log(arrayOfcars);
+
+
+///////////// exercise 4: ////////////
+
+
